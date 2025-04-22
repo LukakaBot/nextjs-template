760f46f95638237edbe3f82e530cb2d59594b06e
鲨鱼辣椒 <monishine_code@163.com>
2025-04-22 09:46:24 +0800

    feat(config): 添加项目根路径配置

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
+const env = process.env;
+
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: env.NEXT_PUBLIC_BASE_PATH,
 };
 
 export default nextConfig;
