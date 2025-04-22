17dda152a5a855d552eb08daaeb14502245c02b7
鲨鱼辣椒 <monishine_code@163.com>
2025-04-22 09:42:57 +0800

    feat(env): 调整环境变量

--- typings/env.d.ts
+++ typings/env.d.ts
@@ -8,5 +8,9 @@ declare namespace NodeJS {
     readonly NEXT_PUBLIC_SERVICE_PREFIX: string;
     /** 存储类型 */
     readonly NEXT_PUBLIC_STORAGE_TYPE: 'localStorage' | 'sessionStorage';
+    /** web socket 地址 */
+    readonly NEXT_PUBLIC_SOCKET_URL: string;
+    /** 项目根路径 */
+    readonly NEXT_PUBLIC_BASE_PATH: string;
   }
 }
